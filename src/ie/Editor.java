6ca803bc913e2ead6496225c4e45f3224885e8bc
abarnaks <abarnadevi.ks@gmail.com>
2019-07-16 19:56:32 -0400

    eecs 3461 project

---
+++ src/ie/Editor.java
@@ -0,0 +1,85 @@
+package ie;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Window.Type;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.BoxLayout;
+import javax.swing.JToolBar;
+import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class Editor {
+
+	private JFrame frmImageeditor;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Editor window = new Editor();
+					window.frmImageeditor.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Editor() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmImageeditor = new JFrame();
+		frmImageeditor.setTitle("ImageEditor 1.0");
+		frmImageeditor.setBounds(100, 100, 812, 457);
+		frmImageeditor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmImageeditor.getContentPane().setLayout(new BoxLayout(frmImageeditor.getContentPane(), BoxLayout.X_AXIS));
+		
+		JPanel panel = new JPanel();
+		frmImageeditor.getContentPane().add(panel);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmImageeditor.setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("File");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open");
+		mnNewMenu.add(mntmOpen);
+		
+		JMenuItem mntmSave = new JMenuItem("Save");
+		mnNewMenu.add(mntmSave);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnNewMenu.add(mntmExit);
+		
+		JMenu mnEdit = new JMenu("Edit");
+		menuBar.add(mnEdit);
+		
+		JMenu mnView = new JMenu("View");
+		menuBar.add(mnView);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+	}
+
+}
--- src/ie/Main.java
+++
@@ -1,5 +0,0 @@
-package ie;
-
-public class Main {
-
-}
